6425cfd8aeb401407d96fad4f399a9495a52cc73
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-09-17 09:54:13 +0800

    feat: create cryptocollapz endpoint

---
+++ routes/CryptoCollapz.py
@@ -0,0 +1,32 @@
+from flask import jsonify, request
+
+from routes import app
+
+
+def convert_price(value):
+    if value % 2 == 0:
+        value = value // 2
+    else:
+        value = value * 3 + 1
+    return value
+
+
+@app.route("/cryptocollapz", methods=["POST"])
+def cryptoCollapz():
+    input_data = request.get_json()["stream"]
+
+    output = []
+    for data_array in input_data:
+        temp_data_array = []
+
+        for data in data_array:
+            temp_price = convert_price(data)
+            max_price = max(data, temp_price)
+
+            while temp_price != 1:
+                temp_price = convert_price(temp_price)
+                max_price = max(max_price, temp_price)
+
+            temp_data_array.append(max_price)
+        output.append(temp_data_array)
+    return jsonify({"output": output})

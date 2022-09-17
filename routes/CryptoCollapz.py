b7a9fc8ebf56663d897a49e5dd7e729267ff636d
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-09-17 11:35:22 +0800

    perf(cryptocollapz): limit while loop

--- routes/CryptoCollapz.py
+++ routes/CryptoCollapz.py
@@ -25,11 +25,12 @@ def cryptoCollapz():
         for data in data_array:
             temp_price = convert_price(data)
             max_price = max(data, temp_price)
-
-            while temp_price not in [0, 1] and temp_price > 0:
+            counter = 0
+            while temp_price not in [0, 1] and temp_price > 0 and counter < 1000000:
                 logging.info("STUCK")
                 temp_price = convert_price(temp_price)
                 max_price = max(max_price, temp_price)
+                counter += 1
 
             temp_data_array.append(max_price)
         output.append(temp_data_array)

0195e2aa354fcaaf7f4d08f1377bbdeabb46ea50
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-09-18 02:57:44 +0800

    perf(swizz): update solution

--- routes/SwizzStig.py
+++ routes/SwizzStig.py
@@ -20,24 +20,17 @@ async def stigwarmup():
         accurate_answers = 0
 
         for i in range(1, max_rating + 1):
-            lower_bound = 1
+            possibleSet = set(range(1, max_rating + 1))
             for question in interview["questions"]:
-
-                # Stig replies
                 is_valid = i >= question["lower"] and i <= question["upper"]
+                valueRange = range(question["lower"], question["upper"] + 1)
+                if not is_valid:
+                    possibleSet = possibleSet.difference(valueRange)
 
                 if is_valid:
-                    lower_bound = max(lower_bound, question["lower"])
+                    possibleSet = possibleSet.intersection(valueRange)
 
-                # if prudent_value == -1:
-                #     prudent_value = 1
-                # else:
-
-                # next_possible_prudent_value = question["lower"]
-                # if is_valid:
-                #     prudent_value = max(next_possible_prudent_value, prudent_value)
-
-            if lower_bound == i:
+            if min(possibleSet) == i:
                 accurate_answers += 1
 
         gcd = Fraction(accurate_answers, denominator)

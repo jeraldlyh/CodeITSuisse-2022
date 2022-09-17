c106958f47a246e66a7d07cca8f36855619be545
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-09-17 18:52:59 +0800

    perf(cauldron): add util function

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+import logging
+
 from dotenv import load_dotenv
 
 from routes import app
@@ -8,3 +10,13 @@ load_dotenv()
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+logging.info("Server has started")
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
-import logging
-
 from App import app
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
 if __name__ == "__main__":
-    logging.info("Server has started")
+
     app.run()
